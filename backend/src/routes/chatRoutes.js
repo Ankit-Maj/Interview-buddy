7b7ddd4c3fa9ed03326755ddab7204e5d660a154
Ankit-Maj <ankitmajumdar17@gmail.com>
2025-12-03 18:21:38 +0530

    minor fix in naming

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { getStreamToken } from "../controllers/chatController.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/token",protectRoute, getStreamToken)
+router.get("/token", protectRoute, getStreamToken);
 
 export default router;
